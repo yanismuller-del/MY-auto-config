54241a17ae85cdd0915d20ae20c4d6e330336a39
yanismuller-del <yanis.muller@icloud.com>
2026-05-06 11:22:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const data = {
+  BMW: {
+    "Serie 3": {
+      "320d 190": {
+        origine: 190,
+        stage1: 230,
+        stage2: 260
+      }
+    }
+  }
+};
+
+function calcul() {
+  let moteur = document.getElementById("moteur").value;
+  let stage = document.getElementById("stage").value;
+
+  let voiture = data["BMW"]["Serie 3"][moteur];
+
+  let resultat = voiture[stage];
+
+  document.getElementById("resultat").innerHTML =
+    `Puissance après modif : ${resultat}ch`;
+}
+function poster() {
+  let titre = document.getElementById("titre").value;
+  let message = document.getElementById("message").value;
+
+  let posts = JSON.parse(localStorage.getItem("posts")) || [];
+
+  posts.push({ titre, message });
+
+  localStorage.setItem("posts", JSON.stringify(posts));
+
+  afficher();
+}
+
+function afficher() {
+  let posts = JSON.parse(localStorage.getItem("posts")) || [];
+
+  let html = "";
+
+  posts.forEach(p => {
+    html += `<h3>${p.titre}</h3><p>${p.message}</p>`;
+  });
+
+  document.getElementById("posts").innerHTML = html;
+}
+
+if (document.getElementById("posts")) {
+  afficher();
+}
